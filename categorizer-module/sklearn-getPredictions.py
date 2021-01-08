055f52ae6ac09a1442df1c0b1d6865113c5fff01
Gabriel Harris <mazen.alhrishy@nielsen.com>
2021-01-08 13:01:03 +0000

    Format using Black

--- categorizer-module/sklearn-getPredictions.py
+++ categorizer-module/sklearn-getPredictions.py
@@ -1,18 +1,18 @@
+import json
 import os
+import sys
 
 import joblib
 import pandas as pd
-import json
-import sys
 
 arguments = sys.argv[1:]
 
 # -s json string
 # -p json path
-arguments = [arguments[i:i+2] for i in range(0, len(arguments), 2)]
-#path = os.path.abspath('sklearn/data/trial-json-products.json') #default for testing
+arguments = [arguments[i : i + 2] for i in range(0, len(arguments), 2)]
+# path = os.path.abspath('sklearn/data/trial-json-products.json') #default for testing
 
-path = os.path.abspath('spacy/data/test.json') #default for testing
+path = os.path.abspath("spacy/data/test.json")  # default for testing
 for argument in arguments:
     if argument[0] == "-s":
         path = argument[1]
@@ -25,96 +25,87 @@ if path == None:
     print("Error")
     exit()
 
-df = pd.read_json(
-    path,
-    orient='records',
-    lines=False,
-).set_index(
-    'pvid',
-).sort_index(
-    ascending=True,
-)
-
-df['category_level_1'] = df['categories'].apply(
-    lambda
-    c: c[0]['description'],
-)
-
-df['category_level_2'] = df['categories'].apply(
-    lambda
-    c: c[1]['description'],
-)
-
-df['regulated_product_name'] = df['languages'].apply(
-    lambda
-    c: c[0]['groupingSets'][0]['attributes']['regulatedProductName']
-)
-
-df['ingredients'] = df['languages'].apply(
-    lambda
-    c: '. '.join(
-        c[0]['groupingSets'][0]['attributes']['ingredients']
+df = (
+    pd.read_json(
+        path,
+        orient="records",
+        lines=False,
+    )
+    .set_index(
+        "pvid",
+    )
+    .sort_index(
+        ascending=True,
     )
 )
 
-df['text'] = df[
-    ['regulated_product_name', 'ingredients']
-].apply(
-    lambda s: '. '.join(s[s.notna()]),
+df["category_level_1"] = df["categories"].apply(
+    lambda c: c[0]["description"],
+)
+
+df["category_level_2"] = df["categories"].apply(
+    lambda c: c[1]["description"],
+)
+
+df["regulated_product_name"] = df["languages"].apply(
+    lambda c: c[0]["groupingSets"][0]["attributes"]["regulatedProductName"]
+)
+
+df["ingredients"] = df["languages"].apply(
+    lambda c: ". ".join(c[0]["groupingSets"][0]["attributes"]["ingredients"])
+)
+
+df["text"] = df[["regulated_product_name", "ingredients"]].apply(
+    lambda s: ". ".join(s[s.notna()]),
     axis=1,
 )
 
-df['storage_env'] = df['languages'].apply(
-    lambda
-    c: c[0]['groupingSets'][0]['attributes']['storageType'][0]
-    ['lookupValue']
+df["storage_env"] = df["languages"].apply(
+    lambda c: c[0]["groupingSets"][0]["attributes"]["storageType"][0]["lookupValue"]
 )
 
-df['pack_type'] = df['languages'].apply(
-    lambda
-    c: c[0]['groupingSets'][0]['attributes']['packType'][0]
-    ['lookupValue']
+df["pack_type"] = df["languages"].apply(
+    lambda c: c[0]["groupingSets"][0]["attributes"]["packType"][0]["lookupValue"]
 )
 
 
 def parse_cooking_guidelines(c):
     try:
         return [
-            item['nameValue']
-            for item in c[0]['groupingSets'][0]['attributes']
-            ['cookingGuidelines']
+            item["nameValue"]
+            for item in c[0]["groupingSets"][0]["attributes"]["cookingGuidelines"]
         ]
 
     except KeyError:
-        return ['None']
+        return ["None"]
 
 
-df['cooking_type'] = df['languages'].apply(
-    parse_cooking_guidelines
-)
+df["cooking_type"] = df["languages"].apply(parse_cooking_guidelines)
 
-df = df[[
-    'category_level_1',
-    'category_level_2',
-    'storage_env',
-    'pack_type',
-    'cooking_type',
-    'text'
-]]
+df = df[
+    [
+        "category_level_1",
+        "category_level_2",
+        "storage_env",
+        "pack_type",
+        "cooking_type",
+        "text",
+    ]
+]
 
 
 scriptPath = os.path.dirname(os.path.realpath(__file__))
-le = joblib.load(os.path.abspath(scriptPath + '/sklearn/models/LabelEncoder.pkl'))
+le = joblib.load(os.path.abspath(scriptPath + "/sklearn/models/LabelEncoder.pkl"))
 
-vc = joblib.load(os.path.abspath(scriptPath + '/sklearn/models/VotingClassifier.pkl'))
+vc = joblib.load(os.path.abspath(scriptPath + "/sklearn/models/VotingClassifier.pkl"))
 
-df['predict'] = le.inverse_transform(vc.predict(df))
+df["predict"] = le.inverse_transform(vc.predict(df))
 
 newArr = list()
-for k,v in df['predict'].items():
+for k, v in df["predict"].items():
     new = dict()
-    new['pvid'] = k
-    new['category'] = v
+    new["pvid"] = k
+    new["category"] = v
     newArr.append(new)
 
 print(json.dumps(newArr))
